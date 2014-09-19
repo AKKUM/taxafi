f8109fde7ba2e95bfd5265f473b4c58a66fc048e
unknown <akhilesh.i.kumar@MW7EXS3M9S3NU2.dir.svc.accenture.com>
2014-09-19 12:52:23 +0100

    more code

---
+++ app/utils/HelloWorld.scala
@@ -0,0 +1,22 @@
+package utils
+
+/**
+ * Created by akhilesh.i.kumar on 18/09/2014.
+ */
+
+class HelloWorld () {
+  def multiplyValue(a:Int, b:Int) : Int =  {
+    a*b
+  }
+}
+/*object HelloWorld {
+
+
+  def main(args: Array[String]) =
+    println("Hello World!")
+
+
+  def multiplyValue(a:Int, b:Int) : Int =  {
+      a*b
+  }
+}*/
---
+++ test/utils/HelloWorldSpec.scala
@@ -0,0 +1,26 @@
+package utils
+
+import org.specs2.mutable._
+import org.specs2.runner._
+import org.junit.runner._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+ * Add your spec here.
+ * You can mock out a whole application including requests, plugins etc.
+ * For more information, consult the wiki.
+ */
+@RunWith(classOf[JUnitRunner])
+class HelloWorldSpec extends Specification {
+
+  "multiply Value" should {
+
+    "retutn the correct value" in new WithApplication{
+      var multiplyService = new HelloWorld()
+      multiplyService.multiplyValue(3,3) must equalTo(9)
+    }
+
+  }
+}
